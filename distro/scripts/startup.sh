94f9792384b25f689d5728ab8b2465e963cbc57e
Смолкин Павел Владимирович <P.Smolkin@otpbank.ru>
2021-12-03 21:18:54 +0300

    revert startup.sh

--- distro/scripts/startup.sh
+++ distro/scripts/startup.sh
@@ -3,14 +3,8 @@
 LOG_PATH="/var/log/wsl-vpnkit.log"
 USERPROFILE=$(wslpath "$(powershell.exe -NoLogo -NoProfile -c 'Write-Host -NoNewline $env:USERPROFILE')")
 VERSION="$(cat /app/version)"
-CONF_PATH="$USERPROFILE/wsl-vpnkit/wsl-vpnkit.conf"
 
 touch $LOG_PATH
-
-mkdir -p "$USERPROFILE/wsl-vpnkit/"
-if [ ! -f "$CONF_PATH" ]; then
-  cp /app/defaults.conf "$CONF_PATH"
-fi
 
 echo "
 wsl-vpnkit $VERSION
@@ -20,13 +14,6 @@ Run the following commands from Windows or other WSL 2 distros to use.
 
     wsl.exe -d $WSL_DISTRO_NAME service wsl-vpnkit start
     wsl.exe -d $WSL_DISTRO_NAME service wsl-vpnkit stop
-
-or just create an alias:
-
-    echo 'alias vpnkit="wsl.exe -d wsl-vpnkit service wsl-vpnkit"' >/etc/profile.d/vpnkit.sh
-    source /etc/profile.d/vpnkit.sh
-    vpnkit start
-    vpnkit stop
 
 The following files will be copied if they do not already exist.
 
