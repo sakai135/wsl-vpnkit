64d3716c998000c13b40eeceb9e9f689381962d4
Jose Sa <jose.sa@nokia.com>
2022-09-10 12:54:30 +0100

    Improve service init script and logs
    - Fixed init service script to allow proper 'start' and 'stop' actions consistently.
      I kept struggling that service start only worked after killing the distro because
      sometimes it didn't start/stop properly leaving processes running and without pid
      file
    - Added service 'status' action
    - Added additional service debug by setting environment variable DEBUG, ie:
      wsl.exe -d wsl-vpnkit DEBUG=1 service wsl-vpnkit stop
    - Changed order in startup welcome screen having stop followed by start,
      this makes it easier for users to just copy/paste from welcome screen to have
      service running
    - Added log check command to see only the latest service execution logs

--- distro/test.sh
+++ distro/test.sh
@@ -7,13 +7,43 @@ USERPROFILE="$(powershell.exe -c 'Write-Host -NoNewline $env:USERPROFILE')"
 DUMP=wsl-vpnkit.tar.gz
 TAG_NAME=wslvpnkit
 
+# build
 docker build -t $TAG_NAME -f ./distro/Dockerfile .
 CONTAINER_ID=$(docker create $TAG_NAME)
 docker export $CONTAINER_ID | gzip > $DUMP
 docker container rm $CONTAINER_ID
 ls -la $DUMP
 
+# reinstall
 wsl.exe --unregister wsl-vpnkit || :
 wsl.exe --import wsl-vpnkit "$USERPROFILE\\wsl-vpnkit" $DUMP --version 2
 rm $DUMP
-wsl.exe -d wsl-vpnkit
+
+# tests
+(
+  # start service
+  set -x
+  wsl.exe -d wsl-vpnkit service wsl-vpnkit start
+  output=$(wsl.exe -d wsl-vpnkit DEBUG=1 service wsl-vpnkit status)
+  echo "$output" | grep "Service wsl-vpnkit is running"
+
+  # check latest log
+  sleep 5
+  output=$(wsl.exe -d wsl-vpnkit sh -c "tac /var/log/wsl-vpnkit.log | awk '{print}; /starting wsl-vpnkit/{exit}' | tac")
+
+  # check for working ping
+  echo "$output" | grep "ping success"
+
+  # check for working dns
+  echo "$output" | grep "nslookup success"
+
+  # stop service
+  wsl.exe -d wsl-vpnkit DEBUG=1 service wsl-vpnkit stop
+  output=$(wsl.exe -d wsl-vpnkit DEBUG=1 service wsl-vpnkit status)||true
+  echo "$output" | grep "Service wsl-vpnkit is not running"
+
+  # check welcome screen
+  wsl.exe -d wsl-vpnkit sh -c 'echo 1 | source /etc/profile'
+)
+
+echo "$0 Finished"
