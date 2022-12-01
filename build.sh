78eccf44c6b409ae6d9f8eee05bfec6516c8287e
Jose Sa <jose.sa@nokia.com>
2022-12-01 08:14:09 +0000

    Fixed wsl-vpnkit.service - stop and restart actions are performed consistently Improved build.sh - able to build inside vpn if using http_proxy

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
-#! /bin/sh -xe
+#! /bin/bash -xe
 
 # run from repo root
 # ./build.sh
 
-USERPROFILE="$(powershell.exe -c 'Write-Host -NoNewline $env:USERPROFILE')"
+USERPROFILE=$(wslvar USERPROFILE)
 DUMP=wsl-vpnkit.tar.gz
 TAG_NAME=wslvpnkit
 
 # build
-docker build -t $TAG_NAME -f ./distro/Dockerfile .
-CONTAINER_ID=$(docker create $TAG_NAME)
-docker export $CONTAINER_ID | gzip > $DUMP
-docker container rm $CONTAINER_ID
-ls -la $DUMP
+build_args=()
+[ -z "${http_proxy}" ] || build_args+=( --build-arg http_proxy="${http_proxy}" )
+[ -z "${https_proxy}" ] || build_args+=( --build-arg https_proxy="${https_proxy}" )
+[ -z "${no_proxy}" ] || build_args+=( --build-arg no_proxy="${no_proxy}" )
+docker build --network host "${build_args[@]}" --tag ${TAG_NAME} --file ./distro/Dockerfile .
+CONTAINER_ID=$(docker create ${TAG_NAME})
+docker export "${CONTAINER_ID}" | gzip > ${DUMP}
+docker container rm "${CONTAINER_ID}"
+ls -la ${DUMP}
 
 # reinstall
 wsl.exe --unregister wsl-vpnkit || :
-wsl.exe --import wsl-vpnkit --version 2 "$USERPROFILE\\wsl-vpnkit" $DUMP
-rm $DUMP
+wsl.exe --import wsl-vpnkit --version 2 "${USERPROFILE}\\wsl-vpnkit" ${DUMP}
+rm ${DUMP}
