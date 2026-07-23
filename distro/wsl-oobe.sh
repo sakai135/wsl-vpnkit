20cc18daa96c0f52b82003425854b3ceccf4e26b
Keiichi Shimamura <sakai135@users.noreply.github.com>
2026-07-23 01:28:34 -0400

    update readme (#277)
    * update order
    
    * add autoProxy
    
    * update readme
    
    * update oobe message

--- distro/wsl-oobe.sh
+++ distro/wsl-oobe.sh
@@ -9,9 +9,7 @@ cat <<EOF
 wsl-vpnkit ${VERSION} ${ARCH}
 
 wsl-vpnkit is now installed as a WSL 2 distro.
-Use the following command from your normal WSL 2 distro to start.
-
-wsl.exe -d wsl-vpnkit --cd /app wsl-vpnkit
+You can close this terminal window.
 
 
 EOF
